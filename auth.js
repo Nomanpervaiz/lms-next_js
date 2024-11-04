528b70742574def7e2d94be3b90e29372bb25730
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-11-04 20:42:37 +0500

    added use client in signin page

--- auth.js
+++ auth.js
@@ -2,84 +2,82 @@ import { connectDB } from "@/lib/dbConnect";
 import userModal from "@/lib/Modals/UserModal";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials"
+import Credentials from "next-auth/providers/credentials";
 
 const handleLogin = async (profile) => {
-    console.log(profile);
-    await connectDB();
-
-    const user = await userModal.findOne({ email: profile.email });
-    if (user) {
-        return user;
-    } else {
-        console.log(profile?.name);
-        const obj = {
-            name: profile?.name,
-            email: profile?.email,
-            provider: "Google",
-            profileImg: profile?.picture,
-        };
-        let newUser = await new userModal(obj);
-        newUser = await newUser.save();
-        return newUser;
-    }
+  console.log(profile);
+  await connectDB();
+  const user = await userModal.findOne({ email: profile.email });
+  if (user) {
+    return user;
+  } else {
+    console.log(profile?.name);
+    const obj = {
+      name: profile?.name,
+      email: profile?.email,
+      provider: "Google",
+      profileImg: profile?.picture,
+    };
+    let newUser = await new userModal(obj);
+    newUser = await newUser.save();
+    return newUser;
+  }
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Google,
-        Credentials({
-            credentials: {
-                email: {},
-                password: {},
-            },
-            authorize: async (credentials) => {
-                let user = null;
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
 
-                console.log("credentials", credentials);
-                let res = await fetch("https://lms-next-js-livid.vercel.app/api/users/login",
-                    {
-                        method: "POST",
-                        body: JSON.stringify({
-                            email: credentials.email,
-                            password: credentials.password,
-                        })
-                    }
-                )
-                res = await res.json()
-                console.log("response ==> ", res.user);
-                user = await res.user
-                return user;
-            }
-        }),
-    ],
-    callbacks: {
-        async signIn({ account, profile }) {
-            console.log("account", account);
+        console.log("credentials", credentials);
+        let res = await fetch(
+          "https://lms-next-js-livid.vercel.app/api/users/login",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: credentials.email,
+              password: credentials.password,
+            }),
+          }
+        );
+        res = await res.json();
+        console.log("response ==> ", res.user);
+        user = await res.user;
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      console.log("account", account);
 
-            if (account.provider == "google") {
-                const user = await handleLogin(profile);
-                return { ...profile, role: user?.role };
-            }
-            return true;
-        },
-        async jwt({ token }) {
-
-
-            const user = await handleLogin(token);
-            console.log("user in jwt ==>", user);
-            token._id = user._id;
-            token.role = user.role;
-            token.image = user.profileImg;
-            token.name = user.name;
-            return token;
-        },
-        session({ session, token }) {
-            session.user._id = token._id;
-            session.user.role = token.role;
-            session.user.profile = token.image;
-            session.user.username = token.name;
-            return session;
-        },
+      if (account.provider == "google") {
+        const user = await handleLogin(profile);
+        return { ...profile, role: user?.role };
+      }
+      return true;
     },
+    async jwt({ token }) {
+      const user = await handleLogin(token);
+      console.log("user in jwt ==>", user);
+      token._id = user._id;
+      token.role = user.role;
+      token.image = user?.profileImg;
+      token.name = user?.name;
+      return token;
+    },
+    session({ session, token }) {
+      session.user._id = token._id;
+      session.user.role = token.role;
+      session.user.profile = token.image;
+      session.user.username = token.name;
+      return session;
+    },
+  },
 });
